64e3ad54f46edb6e74ece96def6aa874040d8318
SantaHirai <pockyou15@gmail.com>
2025-05-07 13:39:14 +0900

    フェードインアニメーションの追加
    フェードインアニメーションの追加とインデントの整頓とか見た目の変化

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 $(function(){
-    var pagetop = $('#page-top');
-    pagetop.hide();
-    $(window).scroll(function () {
-       if ($(this).scrollTop() > 300) {
-            pagetop.fadeIn();
-       } else {
+   var pagetop = $('#page-top');
+   pagetop.hide();
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 300) {
+         pagetop.fadeIn();
+      } else {
             pagetop.fadeOut();
-       }
-    });
-    pagetop.click(function () {
-       $('body, html').animate({ scrollTop: 0 }, 500);
-       return false;
-    });
-  });
+      }
+   });
+   pagetop.click(function () {
+      $('body, html').animate({ scrollTop: 0 }, 500);
+      return false;
+   });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+   const sections = document.querySelectorAll('.section');
+
+   const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+         if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.animation = 'fadeInUp 1s ease forwards';
+            observer.unobserve(entry.target);
+         }
+      });
+   }, {
+      threshold: 0.4
+   });
+
+   sections.forEach(section => {
+      observer.observe(section);
+   });
+});
